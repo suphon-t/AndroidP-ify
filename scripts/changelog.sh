011b27623a63945140d38003b7c387b0732cf4f2
paphonb <paphonb@gmail.com>
2018-03-18 14:02:15 +0700

    Add Travis CI

---
+++ scripts/changelog.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env sh
+MERGE_PREFIX="Merge pull request"
+
+if [ ! -z "$TRAVIS_TAG" ]
+then
+    TRAVIS_COMMIT_RANGE="$(git describe --abbrev=0 --tags ${TRAVIS_TAG}^)..$TRAVIS_TAG"
+fi
+
+GIT_COMMIT_LOG="$(git log --format='%s (by %cn)' ${TRAVIS_COMMIT_RANGE})"
+
+echo " <b>Changelog for build ${MAJOR_MINOR}_${TRAVIS_BUILD_NUMBER}</b>${NEWLINE}"
+
+printf '%s\n' "$GIT_COMMIT_LOG" | while IFS= read -r line
+do
+  echo "- ${line}"
+done
---
+++ scripts/deploy-telegram.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+
+APK_NAME=AndroidPify-${MAJOR_MINOR}_${TRAVIS_BUILD_NUMBER}.apk
+
+cp app/build/outputs/apk/release/app-release.apk ${APK_NAME}
+
+CHANGELOG="$(./scripts/changelog.sh)
+<a href=\"https://github.com/${TRAVIS_REPO_SLUG}/compare/${TRAVIS_COMMIT_RANGE}\">View on GitHub</a>"
+
+curl -F chat_id="-1001259165513" -F sticker="CAADBQADKAADTBCSGmapM3AUlzaHAg" https://api.telegram.org/bot${BOT_TOKEN}/sendSticker
+curl -F chat_id="-1001259165513" -F document=@"${APK_NAME}" https://api.telegram.org/bot${BOT_TOKEN}/sendDocument
+curl -F chat_id="-1001259165513" -F text="${CHANGELOG}" -F parse_mode="HTML" -F disable_web_page_preview="true" https://api.telegram.org/bot${BOT_TOKEN}/sendMessage
+
+echo $(./scripts/changelog.sh)
