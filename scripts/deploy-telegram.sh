8896fa859cdd718e362511e27c38aac7bc2a6371
paphonb <paphonb@gmail.com>
2018-03-18 11:58:23 +0700

    Fix apk signing

--- scripts/deploy-telegram.sh
+++ scripts/deploy-telegram.sh
@@ -2,7 +2,7 @@
 
 APK_NAME=AndroidPify-${MAJOR_MINOR}_${TRAVIS_BUILD_NUMBER}.apk
 
-cp app/build/outputs/apk/release/app-release-unsigned.apk ${APK_NAME}
+cp app/build/outputs/apk/release/app-release.apk ${APK_NAME}
 
 curl -F chat_id="-1001259165513" -F document=@"${APK_NAME}" https://api.telegram.org/bot${BOT_TOKEN}/sendDocument
 curl -F chat_id="-1001259165513" -F text="$(./scripts/changelog.sh)" -F parse_mode="HTML" https://api.telegram.org/bot${BOT_TOKEN}/sendMessage
